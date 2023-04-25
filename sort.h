f3b7b08b5b829698c27d90e8b1ea80a26b85e5cb
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-04-25 13:51:56 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -3,8 +3,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
+
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 /**
  * struct listint_s - Doubly linked list node
@@ -20,12 +33,11 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-
-/* print functions */
+/* Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* sorting algorithms */
+/* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -39,4 +51,4 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
-#endif
+#endif /* SORT_H */
